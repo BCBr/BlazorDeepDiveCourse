b64722174c42cf2d7d8efe4d81ed32f3b429f0b2
BCBr <52011766+BCBr@users.noreply.github.com>
2025-06-01 01:18:08 -0300

    Section 3: Add server interacitvity complete

--- BlazorDeepDive/ServerManagement/obj/Debug/net8.0/ServerManagement.AssemblyInfo.cs
+++ BlazorDeepDive/ServerManagement/obj/Debug/net8.0/ServerManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5814a2203bdf5e4ae3682379a324b9cf3faf7640")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
